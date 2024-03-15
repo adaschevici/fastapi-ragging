6a30ceb7c1848dc184b33f800a8f4c773e4cd88e
Artur Das <a.daschevici@gmail.com>
2024-03-15 15:58:52 +0100

    Fix up config with new pydantic

--- config.py
+++ config.py
@@ -1,5 +1,20 @@
-from pydantic import Field
-from pydantic_settings import BaseSettings
+import os
+from functools import lru_cache
+from pydantic import Field, SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-def get_settings():
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file="config.env", env_file_encoding="utf-8")
+    hf_access_token: SecretStr = Field(alias="HUGGINGFACEHUB_API_TOKEN")
+    openai_api_key: SecretStr = Field(alias="OPENAI_API_KEY")
+
+
+@lru_cache
+def get_settings() -> Settings:
+    settings = Settings()
+    os.environ["HUGGINGFACEHUB_API_TOKEN"] = settings.hf_access_token.get_secret_value()
+    os.environ["OPENAI_API_KEY"] = settings.openai_api_key.get_secret_value()
+    return settings
+
     ...
