30621aa1eb7a551a3fd3504742baa45e79861c34
Artur Das <a.daschevici@gmail.com>
2024-03-17 18:28:23 +0100

    Fix gunicorn startup script, it does not allow a wsgi runner so it needs the uvicorn class as a runner

--- runner.sh
+++ runner.sh
@@ -1,1 +1,1 @@
-gunicorn 'main:create_app()' --bind 0.0.0.0:4500 --workers 4 --threads 4 --worker-class gthread
+gunicorn 'main:create_app()' --bind 0.0.0.0:4500 --workers 4 --worker-class uvicorn.workers.UvicornWorker
