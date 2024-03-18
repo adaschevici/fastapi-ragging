f675aebd448313f05e27ad2d2878f38b3353976f
Artur Das <a.daschevici@gmail.com>
2024-03-18 16:28:58 +0100

    Create function for embeddings

--- embedding_tools.py
+++ embedding_tools.py
@@ -1,18 +1,21 @@
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
-model_name = "jinaai/jina-embeddings-v2-small-en"
-model_kwargs = {"device": "mps", "trust_remote_code": True}
-encode_kwargs = {
-    "normalize_embeddings": False,
-}
-hf = HuggingFaceEmbeddings(
-    model_name=model_name,
-    model_kwargs=model_kwargs,
-    encode_kwargs=encode_kwargs,
-)
-text = "This is a test document."
 
-res = hf.embed_query(text)
+def create_embeddings(text: str):
+    model_name = "jinaai/jina-embeddings-v2-small-en"
+    model_kwargs = {"device": "mps", "trust_remote_code": True}
+    encode_kwargs = {
+        "normalize_embeddings": False,
+    }
+    hf = HuggingFaceEmbeddings(
+        model_name=model_name,
+        model_kwargs=model_kwargs,
+        encode_kwargs=encode_kwargs,
+    )
+    res = hf.embed_query(text)
+    return res
 
 if __name__ == "__main__":
+    text = "I love to code"
+    res = create_embeddings(text)
     print(len(res))
