a2161a054b2853d12c0997995fabc0df330ced96
Artur Das <a.daschevici@gmail.com>
2024-03-18 23:02:29 +0100

    Update settings for qdrant

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 from functools import lru_cache
-from pydantic import Field, SecretStr
+from pydantic import Field, SecretStr, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -8,10 +8,16 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file="config.env", env_file_encoding="utf-8")
     hf_access_token: SecretStr = Field(alias="HUGGINGFACEHUB_API_TOKEN")
     openai_api_key: SecretStr = Field(alias="OPENAI_API_KEY")
-    qdrant_url: str = Field(default="http://localhost:6333", alias="QDRANT_URL")
+    qdrant_host: str = Field(default="localhost", alias="QDRANT_HOST")
+    qdrant_port: int = Field(default=6333, alias="QDRANT_PORT")
     PROJECT_NAME: str = Field(default="integration", alias="PROJECT_NAME")
     VERSION: str = Field(default="0.1.0", alias="PROJECT_VERSION")
     ALLOWED_HOST_ORIGINS: str = Field(default="*", alias="ALLOWED_HOST_ORIGINS")
+
+    @computed_field
+    @property
+    def qdrant_host_port(self) -> str:
+        return f"http://{self.qdrant_host}:{self.qdrant_port}"
 
 
 @lru_cache
