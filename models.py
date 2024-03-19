71d3927ce89bcd386845909fa65f7b98b248a089
Artur Das <a.daschevici@gmail.com>
2024-03-19 14:41:15 +0100

    Add strong types for payloads on the two relevant endpoints

--- models.py
+++ models.py
@@ -2,3 +2,9 @@ from pydantic import BaseModel
 
 class Message(BaseModel):
     msg: str
+
+class CreateIndexPayload(BaseModel):
+    urls: list[str]
+
+class SearchPayload(BaseModel):
+    query: str
--- routes.py
+++ routes.py
@@ -1,22 +1,26 @@
 from fastapi import APIRouter, Depends
-from models import Message
-from starlette.requests import Request
-from bootstrap import get_logger
+from models import Message, CreateIndexPayload, SearchPayload
+from bootstrap import get_logger, get_qdrant_client
 
 router = APIRouter()
 
 
-
 @router.get("/")
-async def root(logger = Depends(get_logger)):
+async def root(logger=Depends(get_logger)):
     logger.info("Hello World")
     logger.debug("Debugging")
     return {"message": "Hello World"}
 
+
 @router.post("/search")
-async def search(query: Message):
+async def search(
+    query: SearchPayload, qdrant_client=Depends(get_qdrant_client), logger=Depends(get_logger)
+):
     return {"message": query}
 
+
 @router.post("/create-index")
-async def create_index():
+async def create_index(
+    urls: CreateIndexPayload, qdrant_client=Depends(get_qdrant_client), logger=Depends(get_logger)
+):
     return {"message": "Index created"}
